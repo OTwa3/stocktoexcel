5cf57b7d723a21565dd4666e16df4b64ff7506e8
Owen Twa <owentwa3@gmail.com>
2024-08-29 11:54:21 -0700

    Init. push

---
+++ src/main/java/com/stocktoexcel/stocktoexcel/StocktoexcelApplication.java
@@ -0,0 +1,13 @@
+package com.stocktoexcel.stocktoexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktoexcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktoexcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocktoexcel/stocktoexcel/StocktoexcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocktoexcel.stocktoexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktoexcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
